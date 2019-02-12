488f17aa1cbc0c9c9cbdcc8490ff1acb0267e30e
Terry <terrytsan@hotmail.com>
2019-02-12 13:17:12 +0000

    Cleaning code

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
 
+#constants
+video = "nt3D26lrkho.mp4"
+videoScaleFactor = 0.5
+
 # Load the video
-cap = cv2.VideoCapture("nt3D26lrkho.mp4")
+cap = cv2.VideoCapture(video)
 
 # # play the video
 # while cap.isOpened():
@@ -16,24 +20,25 @@ cap = cv2.VideoCapture("nt3D26lrkho.mp4")
 # cap.release()
 # cv2.destroyAllWindows()
 
-# subtractor = cv2.bgsegm.createBackgroundSubtractorMOG()
 subtractor = cv2.createBackgroundSubtractorMOG2()
 
 # This kernel will be used to remove noise
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
-
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 # Counter for frames. Outputs the current frame number
 count = 0
 # Play the video
 while (1):
 	ret, frame = cap.read()
 	# resize frame
-	frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+	frame = cv2.resize(frame, (0, 0), fx=videoScaleFactor, fy=videoScaleFactor)
 	# apply guassian blur
-	frame = cv2.GaussianBlur(frame, (5, 5), 0)
+	#frame = cv2.GaussianBlur(frame, (5, 5), 0)
 	
 	foregroundMask = subtractor.apply(frame)
+	#opening removes false positives (white dots in background)
 	foregroundMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
+	#closing removes false negatives (black dots in actual object)
+	foregroundMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_CLOSE, kernel)
 	
 	# erode the frame, removes noise
 	EKernel = np.ones((6,6), np.uint8)
@@ -47,30 +52,23 @@ while (1):
 	ret, frameThresh = cv2.threshold(dilation, 200, 255, cv2.THRESH_TOZERO)
 	
 	# find countours
-	contours, _ = cv2.findContours(foregroundMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	# cv2.drawContours(foregroundMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE, color=(255,255,255))
-	#cv2.drawContours(frameThresh, contours, -1, (255, 255, 0), 3)
 	
 	
-	
-	
-	# This section is on displaying the frames
+	# This section displays the frames
 	
 	# show the two frames side by side (appears to be a video)
 	cv2.imshow('Blob frame', foregroundMask)
 	# get dimensions of window
 	blobX, blobY, blobW, blobH = cv2.getWindowImageRect('Blob frame')
 	cv2.moveWindow('Blob frame', 0, 0)
+	
+	# show the original frame
 	cv2.imshow('Original frame', frame)
 	cv2.moveWindow('Original frame', blobX + blobW, blobY)
 	
 	# show the keypoints
 	cv2.imshow('Threshold', frameThresh)
 	cv2.moveWindow('Threshold', blobX, + blobY + blobH)
-	
-	# print the number of the current frame
-	count = count + 1
-	print(count)
 	
 	if cv2.waitKey(15) == 13:
 		break
