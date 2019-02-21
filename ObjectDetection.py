2dacf1db95d9000d0281efc21251e19d687be524
Terry <terrytsan@hotmail.com>
2019-02-21 01:30:37 +0000

    Improve comments

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -44,21 +44,22 @@ while 1:
 	# resize frame
 	frame = cv2.resize(frame, (0, 0), fx=videoScaleFactor, fy=videoScaleFactor)
 	
-	subKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
-	
+	# Background subtraction
 	foregroundMask = subtractor.apply(frame, None, -1)
 	# opening removes false positives (white dots in background - the noise)
 	foregroundMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
 	# closing removes false negatives (black dots in actual object)
+	subKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
 	foregroundMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_CLOSE, subKernel)
 	
 	# threshold the frame - removes the random large changes
 	ret, frameThresh = cv2.threshold(foregroundMask, 200, 255, cv2.THRESH_TOZERO)
 	
-	# erode the frame, removes noise
+	# Further noise reduction/dilation
 	EKernel = np.ones((2, 2), np.uint8)
 	DKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 	# erosion = cv2.erode(frameThresh, EKernel, iterations=1)  # gets rid of things
+	# try and fill the gaps in objects
 	dilation = cv2.dilate(frameThresh, DKernel, iterations=5)  # makes things more pronounced
 	
 	# This section displays the frames
